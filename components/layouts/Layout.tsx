ee0c61f1d795d9795c3f74798a1bffc0a103d90c
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 09:38:50 -0300

    Add ThemeChangerPage UI

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,11 +1,22 @@
 import { FC, PropsWithChildren } from "react";
+
 import Head from "next/head";
+
 import { Navbar } from "../ui";
 
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
+interface Props {
+  title?: string;
+}
+
+export const Layout: FC<PropsWithChildren<Props>> = ({
+  children,
+  title = "CookieMaster",
+}) => {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <nav>
         <Navbar />
       </nav>
--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,38 @@
+import { useState, ChangeEvent } from "react";
+
 import { Layout } from "@/components/layouts";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 
 function ThemeChangerPage() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const onThemeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurrentTheme(e.target.value);
+  };
+
   return (
     <Layout>
-      <h1>ThemeChangerPage</h1>
+      <Card>
+        <CardContent>
+          <FormControl>Theme</FormControl>
+          <RadioGroup value={currentTheme} onChange={onThemeChange}>
+            <FormControlLabel value="light" control={<Radio />} label="Light" />
+            <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+            <FormControlLabel
+              value="custom"
+              control={<Radio />}
+              label="Custom"
+            />
+          </RadioGroup>
+        </CardContent>
+      </Card>
     </Layout>
   );
 }
